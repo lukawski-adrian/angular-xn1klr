2426cc7b1c20e85b2ffb9733c31fa90972bca4df
lukawski-adrian <adrian.lukawski@gmail.com>
2022-04-07 20:18:50 +0200

    13 files modified, 1 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { TempTableComponent } from './temp-table/temp-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from '../material.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([{ path: '', component: TempTableComponent }]),
+    MaterialExampleModule,
+    BrowserAnimationsModule,
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent,
     ProductAlertsComponent,
     TempTableComponent,
   ],
--- src/app/temp-table/temp-table.component.ts
+++ src/app/temp-table/temp-table.component.ts
@@ -1,34 +1,58 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
-export interface PeriodicElement {
+export interface TempElement {
   name: string;
   position: number;
-  weight: number;
+  temp: number;
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA: TempElement[] = [
+  { position: 1, name: 'Hydrogen', temp: 1.0079, symbol: '°C' },
+  { position: 2, name: 'Helium', temp: 4.0026, symbol: '°C' },
+  { position: 3, name: 'Lithium', temp: 6.941, symbol: '°C' },
+  { position: 4, name: 'Beryllium', temp: 9.0122, symbol: '°F' },
+  { position: 5, name: 'Boron', temp: 10.811, symbol: '°C' },
+  { position: 6, name: 'Carbon', temp: 12.0107, symbol: '°C' },
+  { position: 7, name: 'Nitrogen', temp: 14.0067, symbol: '°F' },
+  { position: 8, name: 'Oxygen', temp: 15.9994, symbol: '°F' },
+  { position: 9, name: 'Fluorine', temp: 18.9984, symbol: '°F' },
+  { position: 10, name: 'Neon', temp: 20.1797, symbol: '°F' },
 ];
 
 /**
  * @title Basic use of `<table mat-table>`
  */
 @Component({
-  selector: 'temp-table',
-  styleUrls: ['temp-table.component.css'],
-  templateUrl: 'temp-table.component.html',
+  selector: 'app-temp-table',
+  styleUrls: ['./temp-table.component.css'],
+  templateUrl: './temp-table.component.html',
 })
-export class TempTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class TempTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['position', 'name', 'temp', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
